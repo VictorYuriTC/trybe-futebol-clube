8c4b3ff581b139f29ed47d4ad517d87f05d3ea1c
Victor Yuri <victoryuritc@yahoo.com>
2023-01-12 15:14:37 -0300

    feat: implement TeamsValidation and TeamsController on TeamsRouter

--- app/backend/src/database/routers/TeamsRouter.ts
+++ app/backend/src/database/routers/TeamsRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import TeamsController from '../controllers/TeamsController';
+import TeamsValidation from '../validations/TeamsValidation';
 
 const TeamsRouter = Router();
 
 TeamsRouter.get(
   '/',
   TeamsController.getAllTeams,
+);
+
+TeamsRouter.get(
+  '/:id',
+  TeamsValidation.validateAllFieldsAreFilled,
+  TeamsController.getTeamById,
 );
 
 export default TeamsRouter;
