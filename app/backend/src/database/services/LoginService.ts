28ee918c81150ae1adaf058d092f834a0ba27554
Victor Yuri <victoryuritc@yahoo.com>
2023-01-12 12:37:00 -0300

    feat: generateJwtToken on LoginService & fix findOne
    findOne by email and compare hashed password with req.body.password usinbg bcryptjs

--- app/backend/src/database/services/LoginService.ts
+++ app/backend/src/database/services/LoginService.ts
@@ -1,22 +1,20 @@
+import { compare } from 'bcryptjs';
 import ILogin from '../interfaces/ILogin';
+import generateJwtToken from '../jwt/generateJwtToken';
 import Users from '../models/Users';
 
 export default class LoginService {
-  static async login({ password, username }: ILogin) {
-    const loggedUser = Users
-      .findOne(
-        {
-          where:
-          {
-            username,
-            password,
-          },
-        },
-      );
-    const isThisPasswordCorrect = true;
+  static async login({ password, email }: ILogin) {
+    const loggedUser = await Users
+      .findOne({ where: { email } });
+
+    if (!loggedUser) return { message: 'User doesn\'t exist' };
+
+    const isThisPasswordCorrect = await compare(password, loggedUser?.password);
 
     if (isThisPasswordCorrect) {
-      return { loggedUser };
+      const token = generateJwtToken({ email });
+      return token;
     }
   }
 }
