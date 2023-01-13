9221f48f34d2840884ad9dd7386400be78cce2bd
Victor Yuri <victoryuritc@yahoo.com>
2023-01-13 14:32:38 -0300

    feat: create getMatchesFilteredByInProgress on service

--- app/backend/src/database/services/MatchesService.ts
+++ app/backend/src/database/services/MatchesService.ts
@@ -7,4 +7,23 @@ export default class MatchesService {
       .findAll({ include: [{ model: Teams, as: 'teamHome' }, { model: Teams, as: 'teamAway' }] });
     return { status: 200, allMatches, message: 'All matches found successfully' };
   }
+
+  static async getMatchesFilteredByInProgress(inProgressStr: string) {
+    const inProgress = inProgressStr === 'true' ? 1 : 0;
+
+    const allMatchesInProgress = await Matches
+      .findAll(
+        {
+          include:
+          [
+            { model: Teams, as: 'teamHome' },
+            { model: Teams, as: 'teamAway' },
+          ],
+
+          where: { inProgress },
+        },
+      );
+
+    return { status: 200, allMatchesInProgress, message: 'Matches successfully found' };
+  }
 }
