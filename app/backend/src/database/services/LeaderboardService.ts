16c77bf8d5c9c8efc0eabc0bab345fc6585658a0
Victor Yuri <victoryuritc@yahoo.com>
2023-01-13 19:55:47 -0300

    feat: except effiency, getMatches and Goals stats on LeaderboardService

--- app/backend/src/database/services/LeaderboardService.ts
+++ app/backend/src/database/services/LeaderboardService.ts
@@ -2,22 +2,22 @@ import TeamsService from './TeamsService';
 
 export default class LeaderboardService {
   static async getLeaderboard() {
-    const { allTeams } = await TeamsService
-      .getAllTeams();
+    const { allTeams } = await TeamsService.getAllTeams();
 
     const allTeamsTotalMatchesPromise = allTeams.map(async (team) => {
-      const { totalGames } = await TeamsService.getTeamTotalMatchesById(team.id);
-      const {
-        totalVictories,
-        totalDraws,
-        totalLosses,
-      } = await TeamsService.getTeamLossesDrawsAndVictoriesById(team.id);
-      return {
-        id: team.id, name: team.teamName, totalGames, totalVictories, totalDraws, totalLosses };
+      const totalMatchesData = await TeamsService.getTeamTotalMatchesById(team.id);
+      const matchesResultsData = await TeamsService
+        .getTeamLossesDrawsAndVictoriesById(team.id);
+      const goalsData = await TeamsService
+        .getTeamGoalsFavorOwnAndBalanceById(team.id);
+      return { id: team.id,
+        name: team.teamName,
+        ...totalMatchesData,
+        ...matchesResultsData,
+        ...goalsData,
+      };
     });
-
     const allTeamsTotalMatches = await Promise.all(allTeamsTotalMatchesPromise);
-
     return { status: 200, message: 'Leaderboard successfully found', allTeamsTotalMatches };
   }
 }
