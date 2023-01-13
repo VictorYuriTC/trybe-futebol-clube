8cfd53542e1faf8a18fa626c6991afc9a8dcaa67
Victor Yuri <victoryuritc@yahoo.com>
2023-01-13 16:42:11 -0300

    refactor: validateAllFieldsAreFilled at LoginValidation
    requirement was not passing on GitHub but it was okay on VSCode. comitting different version to test it again

--- app/backend/src/database/validations/LoginValidation.ts
+++ app/backend/src/database/validations/LoginValidation.ts
@@ -4,7 +4,11 @@ class LoginValidation {
   static validateAllFieldsAreFilled = async (req: Request, res: Response, next: NextFunction) => {
     const { password, email } = req.body;
 
-    if (!password || !email) {
+    if (!password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    if (!email) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
 
