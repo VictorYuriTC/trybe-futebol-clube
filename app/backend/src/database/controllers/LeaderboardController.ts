5d28d897c07b05488e906d2cfca4f4dfef28f757
Victor Yuri <victoryuritc@yahoo.com>
2023-01-14 11:30:07 -0300

    feat: create getLeaderboardForAwayTeam at controller
    also creates orderTeamsPerPointsVictoriesAndGoals auxiliary function

--- app/backend/src/database/controllers/LeaderboardController.ts
+++ app/backend/src/database/controllers/LeaderboardController.ts
@@ -1,29 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 
+async function orderTeamsPerPointsVictoriesAndGoals(teamsArray: any) {
+  const orderedTeamsTotalMatches = await teamsArray
+    .sort((a: any, b: any) => {
+      if (a.totalPoints === b.totalPoints
+        && a.goalsBalance === b.goalsBalance
+        && a.goalsFavor === b.goalsFavor) {
+        return a.goalsOwn - b.goalsOwn;
+      }
+
+      if (a.totalPoints === b.totalPoints
+        && a.goalsBalance === b.goalsBalance) {
+        return b.goalsFavor - a.goalsFavor;
+      }
+
+      if (a.totalPoints === b.totalPoints) {
+        return b.goalsBalance - a.goalsBalance;
+      }
+
+      return b.totalPoints - a.totalPoints;
+    });
+
+  return orderedTeamsTotalMatches;
+}
+
 export default class LeaderboardController {
   static async getLeaderboardForHomeTeam(req: Request, res: Response, _next: NextFunction) {
     const { status, allTeamsTotalMatches } = await LeaderboardService.getLeaderboardForHomeTeam();
 
-    const orderedTeamsTotalMatches = allTeamsTotalMatches
-      .sort((a, b) => {
-        if (a.totalPoints === b.totalPoints
-          && a.goalsBalance === b.goalsBalance
-          && a.goalsFavor === b.goalsFavor) {
-          return a.goalsOwn - b.goalsOwn;
-        }
+    const orderedTeamsTotalMatches = await
+    orderTeamsPerPointsVictoriesAndGoals(allTeamsTotalMatches);
 
-        if (a.totalPoints === b.totalPoints
-          && a.goalsBalance === b.goalsBalance) {
-          return b.goalsFavor - a.goalsFavor;
-        }
+    return res.status(status).json(orderedTeamsTotalMatches);
+  }
 
-        if (a.totalPoints === b.totalPoints) {
-          return b.goalsBalance - a.goalsBalance;
-        }
+  static async getLeaderboardForAwayTeam(req: Request, res: Response, _next: NextFunction) {
+    const { status, allTeamsTotalMatches } = await LeaderboardService.getLeaderboardForAwayTeam();
 
-        return b.totalPoints - a.totalPoints;
-      });
+    const orderedTeamsTotalMatches = await
+    orderTeamsPerPointsVictoriesAndGoals(allTeamsTotalMatches);
 
     return res.status(status).json(orderedTeamsTotalMatches);
   }
