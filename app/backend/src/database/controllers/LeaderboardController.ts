2ed639a03b8cd2a4fdb6d4e72f90dea0a29604dc
Victor Yuri <victoryuritc@yahoo.com>
2023-01-13 21:02:03 -0300

    feat: order and return teams at LeaderboardController

--- app/backend/src/database/controllers/LeaderboardController.ts
+++ app/backend/src/database/controllers/LeaderboardController.ts
@@ -2,16 +2,29 @@ import { NextFunction, Request, Response } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
-  static async getLeaderboard(
-    req: Request,
-    res: Response,
-    _next: NextFunction,
-  ) {
-    const {
-      status,
-      allTeamsTotalMatches,
-    } = await LeaderboardService.getLeaderboard();
+  static async getLeaderboard(req: Request, res: Response, _next: NextFunction) {
+    const { status, allTeamsTotalMatches } = await LeaderboardService.getLeaderboard();
 
-    return res.status(status).json(allTeamsTotalMatches);
+    const orderedTeamsTotalMatches = allTeamsTotalMatches
+      .sort((a, b) => {
+        if (a.totalPoints === b.totalPoints
+          && a.goalsBalance === b.goalsBalance
+          && a.goalsFavor === b.goalsFavor) {
+          return a.goalsOwn - b.goalsOwn;
+        }
+
+        if (a.totalPoints === b.totalPoints
+          && a.goalsBalance === b.goalsBalance) {
+          return b.goalsFavor - a.goalsFavor;
+        }
+
+        if (a.totalPoints === b.totalPoints) {
+          return b.goalsBalance - a.goalsBalance;
+        }
+
+        return b.totalPoints - a.totalPoints;
+      });
+
+    return res.status(status).json(orderedTeamsTotalMatches);
   }
 }
