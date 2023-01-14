f9533e4275f22c7506ab9f248ce4f4712a404e32
Victor Yuri <victoryuritc@yahoo.com>
2023-01-13 21:01:13 -0300

    feat: implement getTeamEfficiency at LeaderboardService

--- app/backend/src/database/services/LeaderboardService.ts
+++ app/backend/src/database/services/LeaderboardService.ts
@@ -6,18 +6,22 @@ export default class LeaderboardService {
 
     const allTeamsTotalMatchesPromise = allTeams.map(async (team) => {
       const totalMatchesData = await TeamsService.getTeamTotalMatchesById(team.id);
-      const matchesResultsData = await TeamsService
-        .getTeamLossesDrawsAndVictoriesById(team.id);
-      const goalsData = await TeamsService
-        .getTeamGoalsFavorOwnAndBalanceById(team.id);
-      return { id: team.id,
+      const matchesResultsData = await TeamsService.getTeamLossesDrawsAndVictoriesById(team.id);
+      const goalsData = await TeamsService.getTeamGoalsFavorOwnAndBalanceById(team.id);
+      const efficiencyData = await TeamsService.getTeamEfficiencyById(team.id);
+      const { totalPoints } = await TeamsService.getTeamTotalPointsById(team.id);
+
+      return {
         name: team.teamName,
+        totalPoints,
         ...totalMatchesData,
         ...matchesResultsData,
         ...goalsData,
+        ...efficiencyData,
       };
     });
     const allTeamsTotalMatches = await Promise.all(allTeamsTotalMatchesPromise);
+
     return { status: 200, message: 'Leaderboard successfully found', allTeamsTotalMatches };
   }
 }
