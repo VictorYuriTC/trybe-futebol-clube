bbc52168805d0c7421fefcd2e4ac4246a5818f5c
Victor Yuri <victoryuritc@yahoo.com>
2023-01-14 11:28:50 -0300

    feat: create getLeaderboardForAwayTeam on LeaderboardService

--- app/backend/src/database/services/LeaderboardService.ts
+++ app/backend/src/database/services/LeaderboardService.ts
@@ -24,4 +24,28 @@ export default class LeaderboardService {
 
     return { status: 200, message: 'Leaderboard successfully found', allTeamsTotalMatches };
   }
+
+  static async getLeaderboardForAwayTeam() {
+    const { allTeams } = await TeamsService.getAllTeams();
+
+    const allTeamsTotalMatchesPromise = allTeams.map(async (team) => {
+      const totalMatchesData = await TeamsService.getTeamTotalMatchesById(team.id, 'away');
+      const matchesResultsData = await TeamsService
+        .getTeamLossesDrawsAndVictoriesById(team.id, 'away');
+      const goalsData = await TeamsService.getTeamGoalsFavorOwnAndBalanceById(team.id, 'away');
+      const efficiencyData = await TeamsService.getTeamEfficiencyById(team.id, 'away');
+      const { totalPoints } = await TeamsService.getTeamTotalPointsById(team.id, 'away');
+
+      return { name: team.teamName,
+        totalPoints,
+        ...totalMatchesData,
+        ...matchesResultsData,
+        ...goalsData,
+        ...efficiencyData,
+      };
+    });
+    const allTeamsTotalMatches = await Promise.all(allTeamsTotalMatchesPromise);
+
+    return { status: 200, message: 'Leaderboard successfully found', allTeamsTotalMatches };
+  }
 }
